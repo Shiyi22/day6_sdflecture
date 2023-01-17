15623fec17530725a1906156ee0cdbf10a158197
Shiyi <yeoshiyi22@gmail.com>
2023-01-17 11:03:21 +0800

    Basic Threading example

---
+++ day6lecture/src/main/java/sg/edu/nus/iss/App.java
@@ -0,0 +1,35 @@
+package sg.edu.nus.iss;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        
+        Thread thread1 = new Thread(new Runnable() {
+            
+            @Override
+            public void run() {
+                for (int i = 0; i < 20; i++) {
+                    System.out.println(Thread.currentThread().getName() + 
+                    " \tRunnable ..." + i);
+                }
+            }
+        }); // note the difference in brackets format 
+        thread1.start(); 
+
+        MyRunnableImplementation mRI = new MyRunnableImplementation(); 
+        Thread thread2 = new Thread(mRI); 
+        thread2.start(); 
+
+        Thread thread3 = new Thread(mRI); 
+        thread3.start(); 
+    }
+}
